f96ed965cf40953a7b8d7ecfbb9c7796f7695dde
Ruel Paya <ruel.paya@gmail.com>
2024-03-06 20:06:42 +0800

    Add changes in the src directory

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,22 @@
+function getRondomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const btnStart = document .querySelector(`[data-start]`);
+console.log(btnStart);
+const btnStop = document.querySelector(`[data-stop]`);
+console.log(btnStop);
+const bodyColor = document.querySelector(`body`);
+let timerId = null;
+
+btnStart.addEventListener(`click`, () => {
+    timerId = setInterval(() => {
+        bodyColor.style.backgroundColor = getRondomHexColor()
+    }, 1000);
+    btnStart.disabled = true;
+    });
+
+    btnStop.addEventListener(`click`, () => {
+        clearInterval (timerId);
+        btnStart.disabled = false;
+    })
